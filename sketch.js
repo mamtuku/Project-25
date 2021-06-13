39789e01330c0c8cb5bbd5056caa089922b1f088
mamtuku <83650530+mamtuku@users.noreply.github.com>
2021-06-13 19:54:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -6,21 +5,21 @@ const Body = Matter.Body;
 const Render = Matter.Render;
 var dustbinObj,groundObject	
 var world;
-
+var paper;
 
 function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
-
+    
 
 	engine = Engine.create();
 	world = engine.world;
 	
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-
+     paper=new Paper(200,450,70);
 	Engine.run(engine);
-  
+   
 }
 
 
@@ -31,5 +30,11 @@ function draw() {
 
   groundObject.display();
   dustbinObj.display();
+ paper.display();
+}
 
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:130,y:-145});
+	}
 }
